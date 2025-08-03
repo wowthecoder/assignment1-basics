eea77e31dbd86e585711cc3ca82e65558dab8225
wowthecoder <wengloo135@gmail.com>
2025-08-03 22:52:56 +0100

    feat: implement rms norm

---
+++ cs336_basics/rmsnorm.py
@@ -0,0 +1,25 @@
+import torch 
+import torch.nn as nn
+from einops import reduce, einsum
+
+class RMSNorm(nn.Module):
+    def __init__(self,
+        d_model: int, # Hidden dimension of the model
+        eps: float = 1e-5, # Epsilon value for numerical stability
+        device: torch.device | None = None, # Device to store the parameters on
+        dtype: torch.dtype | None = None # Data type of the parameters
+    ):
+        super().__init__()
+        self.d_model = d_model 
+        self.eps = eps 
+        self.device = device 
+        self.dtype = dtype 
+        self.gains = nn.Parameter(torch.ones(d_model))
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        squared_sum = reduce(x ** 2, '... d_model -> ...', 'sum')
+        expr = (squared_sum / self.d_model) + self.eps
+        rms = torch.sqrt(expr)
+        prod = einsum(x, self.gains, '... d_model, d_model -> ... d_model')
+        # unsqueeze rms to dimension (..., 1)
+        return prod / rms.unsqueeze(-1)
--- tests/adapters.py
+++ tests/adapters.py
@@ -13,6 +13,7 @@ from cs336_basics.tokenizer import Tokenizer
 from cs336_basics.train_bpe_tokenizer import train_bpe
 from cs336_basics.linear import Linear
 from cs336_basics.embedding import Embedding
+from cs336_basics.rmsnorm import RMSNorm
 
 def run_linear(
     d_in: int,
@@ -386,7 +387,10 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rms_normalizer = RMSNorm(d_model, eps)
+    rms_normalizer.load_state_dict({'gains': weights})
+    output = rms_normalizer(in_features)
+    return output
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
